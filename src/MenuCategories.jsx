c6064d71e9e3b5ca5816ecc29ee0aa06c6bbf69d
Teresa <ttan@gmail.com>
2023-06-03 17:27:49 -0700

    categories styling done

--- src/MenuCategories.jsx
+++ src/MenuCategories.jsx
@@ -6,13 +6,13 @@ class MenuCategories extends Component {
         return(
             <React.Fragment>
                 <ul className="list-group">
-                    <li className="list-group-item active" aria-current="true">BREAD</li>
-                    <li className="list-group-item">CAKES</li>
-                    <li className="list-group-item">COOKIES</li>
-                    <li className="list-group-item">PASTRIES</li>
-                    <li className="list-group-item">SANDWICHES</li>
-                    <li className="list-group-item">COFFEE + TEA</li>
-                    <li className="list-group-item">SPECIALTY</li>
+                    <a href='#'><li className="list-group-item active rounded" aria-current="true">BREAD</li></a>
+                    <a href='#'><li className="list-group-item rounded">CAKES</li></a>
+                    <a href='#'><li className="list-group-item rounded">COOKIES</li></a>
+                    <a href='#'><li className="list-group-item rounded">PASTRIES</li></a>
+                    <a href='#'><li className="list-group-item rounded">SANDWICHES</li></a>
+                    <a href='#'><li className="list-group-item rounded">COFFEE + TEA</li></a>
+                    <a href='#'><li className="list-group-item rounded">SPECIALTY</li></a>
                 </ul>
             </React.Fragment>
         )
