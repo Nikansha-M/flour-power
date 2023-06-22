8616c3fca3ae04598ac0336c5039a5e79743ce1a
Nikansha Maharaj <Nikansha.M@gmail.com>
2023-06-22 04:02:19 -0700

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,26 +1,45 @@
 import './Footer.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
     return (
         <div className="footer">
-            <ul>
-                <li>&#169;2023</li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact Us</a></li>
-                <li><a href="/faq">FAQ</a></li>
-            </ul>
-            <ul className="social">
-                <li><a href="/fb">Facebook</a></li>
-                <li><a href="/ig">Instagram</a></li>  
-                <li><a href="/twitter">Twitter</a></li>
-            </ul>   
-            <ul>
-                <li><a href="/privacy">Privacy Policy</a></li>
-                <li><a href="/terms">Terms of Service</a></li>    
-                <li><a href="/personal-information">Do Not Share My Personal Information</a></li>
-            </ul>
+            <div className="container">
+                <div className="row">
+                    {/* Column 1 */}
+                    <ul>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Location & Hours</a></li>
+                        <li><a href="/faq">FAQ</a></li>
+                    </ul>
+
+                    {/* Column 2 - stay connected*/}
+                    <ul className="social">
+                        {/* <li><a href="/fb"><FontAwesomeIcon icon={faFacebook} /></a></li>
+                <li><a href="/ig"><FontAwesomeIcon icon={faInstagram} /></a></li>  
+                <li><a href="/twitter"><FontAwesomeIcon icon={faTwitter} /></a></li> */}
+                    </ul>
+
+                    {/* Bottom Row */}
+                    <ul>
+                        <li>&copy;{new Date().getFullYear()} Flour Power</li>
+                        <li><a href="/privacy">Privacy Policy</a></li>
+                        <li><a href="/terms">Terms of Service</a></li>
+                        <li><a href="/personal-information">Do Not Share My Personal Information</a></li>
+                    </ul>
+                </div>
+
+                {/* <div className="row">
+                    <p className="col-sm">
+                        &copy;{new Date().getFullYear()} Flour Power
+                    </p>
+                </div> */}
+            </div>
         </div>
     );
 }
+
+export default Footer;
 
 export default Footer;
