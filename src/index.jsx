cc143a254d7527ef896e5d0d2f4314c83307aedd
Teresa <ttan@gmail.com>
2023-06-22 11:59:26 -0700

    front end rendering

--- src/index.jsx
+++ src/index.jsx
@@ -8,13 +8,6 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
