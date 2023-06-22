1c642d7eca7cb42bb429dbe67182ac36431143d9
Nikansha Maharaj <Nikansha.M@gmail.com>
2023-06-22 04:03:04 -0700

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,30 @@
-import './Navbar.css';
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
 
 function Navbar() {
     return (
         <div className="nav">
-            <a href="/" className="image-cropper"><img src="Flour-Power-Logo-whitebg.png" className="logo" alt="Sweet Dreams Bakery and Cafe logo of a cake in cake stand" /></a>
+            <a href="/" className="logo-container">
+                <img src="Flour-Power-Logo-whitebg.png" className="logo" alt="Sweet Dreams Bakery and Cafe logo" />
+            </a>
             <ul>
-                <li><a href="/home">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/menu">Menu</a></li>
-                <li><a href="/contact">Contact Us</a></li>
-                <li><a href="/login">Sign In</a></li>
-                <li><a href="/cart">Shopping Cart</a></li>
+                <li>
+                    <a href="/home">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/menu">Menu</a>
+                </li>
+                <li>
+                    <a href="/account"><FontAwesomeIcon icon={faUser} /></a>
+                </li>
+                <li>
+                    <a href="/cart"><FontAwesomeIcon icon={faCartShopping} /></a>
+                </li>
             </ul>
         </div>
     );
