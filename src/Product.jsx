70f5a891d11a2d1241c529674d50af17a554ebdb
Teresa <ttan@gmail.com>
2023-09-05 13:38:01 -0700

    product cards aligned

--- src/Product.jsx
+++ src/Product.jsx
@@ -5,17 +5,15 @@ export default class Product extends Component {
     render() {
         return (
             <div className='col-lg-3'>
-                <div className='card m-2'>
-                <div className='card-body'>
-                {/* <div className='text-muted'>#{this.props.id}</div> */}
-                <h5 className='pt-5 border-top'>{this.props.productName}</h5>
-                <div>
-                <img src={this.props.photo} />
-                </div>
-                <div>${this.props.price}  |  {this.props.weight}</div>
+                <div className="card h-100 border-primary">
+                    <div className='card-body'>
+                        {/* <div className='text-muted'>#{this.props.id}</div> */}
+                        <h5 className="card-title">{this.props.productName}</h5>
+                    <div><img className='card-photo' src={this.props.photo} /></div>
+                    <div className="card-text">${this.props.price}    {this.props.weight}</div>
                 </div>
                 </div>
-                </div>
+            </div>
         );
     }
 }
